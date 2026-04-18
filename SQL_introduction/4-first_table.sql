f514b69941d279b3e9cb6f23a7fbf699851af7c1
resulkazimov <rsulkazimov29@gmail.com>
2026-04-18 18:34:31 +0000

    Add script to create first_table with id and name

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table called first_table with id and name
+-- SQL query to create a table in the current database
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
