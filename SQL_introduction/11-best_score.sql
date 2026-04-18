09c945a048e53e7ce5cf8cbe2bdd5977759f3df3
resulkazimov <rsulkazimov29@gmail.com>
2026-04-18 18:41:58 +0000

    Add script that lists records with score >= 10 in second_table

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Lists all records with a score >= 10 in the table second_table
+-- SQL query to list score and name, filtered and ordered
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
