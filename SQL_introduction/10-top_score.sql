c1d74df813468f2d8c8da59f3fcafe121a76ccbe
resulkazimov <rsulkazimov29@gmail.com>
2026-04-18 18:41:23 +0000

    Add script that lists all records of second_table ordered by score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Lists all records of the table second_table ordered by score
+-- SQL query to list score and name, ordered by score descending
+SELECT score, name FROM second_table ORDER BY score DESC;
