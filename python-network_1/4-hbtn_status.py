3bceb8be4e24d663bc0722a54993e4abcf80a4de
resulkazimov <rsulkazimov29@gmail.com>
2026-04-25 18:05:17 +0000

    Fix pycodestyle: remove whitespace on blank line

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     headers = {'cfclearance': 'true'}
     response = requests.get(url, headers=headers)
-    
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
