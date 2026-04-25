f3a76ae8812d6026cb1c9ebee57baa320931fb01
resulkazimov <rsulkazimov29@gmail.com>
2026-04-25 18:04:28 +0000

    Add Task 4: fetch status with requests

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Fetches https://intranet.hbtn.io/status using the requests package.
+"""
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    headers = {'cfclearance': 'true'}
+    response = requests.get(url, headers=headers)
+    
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
