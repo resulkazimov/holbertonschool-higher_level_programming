12117b29b8afa61615c75d0c1a1ffc8c59636f88
resulkazimov <rsulkazimov29@gmail.com>
2026-04-25 18:03:32 +0000

    Add Task 3: handle urllib HTTPError

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Sends a request to a URL and displays the body of the response.
+Handles HTTP errors and prints the error code.
+"""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    req.add_header('cfclearance', 'true')
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
